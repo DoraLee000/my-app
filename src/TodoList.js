32c547d5201a28689348c1552c8f96f4a99f53cf
Doralee <doralee@starlux-airlines.com>
2020-05-27 17:55:04 +0800

    redux

--- src/TodoList.js
+++ src/TodoList.js
@@ -8,6 +8,32 @@ class TodoList extends Component {
   constructor(props) {
     super(props);
     this.state = store.getState();
+    store.subscribe(this.storeChange)
+    
+  }
+  storeChange=()=>{
+    this.setState(store.getState())
+  }
+  changeInputValue = (e) =>{
+    const actions = {
+      type:'ChangeData',
+      value:e.target.value
+    }
+    store.dispatch(actions)
+  }
+
+  addItem = () =>{
+    store.dispatch({
+      type:'AddItem'
+    })
+  }
+
+  removeItem = (index) =>{
+    console.log('index',index)
+    store.dispatch({
+      type:'removeItem',
+      index
+    })
   }
   render() {
     return (
@@ -15,15 +41,17 @@ class TodoList extends Component {
         <div>
           <Input
             placeholder={this.state.inputValue}
+            value={this.state.inputValue}
             style={{ width: "250px" }}
+            onChange={()=>{this.changeInputValue()}}
           />
-          <Button type="primary">ADD</Button>
+          <Button type="primary" onClick={()=>{this.addItem()}}>ADD</Button>
         </div>
         <div style={{ width: "300px" }}>
           <List
             bordered
             dataSource={this.state.list}
-            renderItem={(item) => <List.Item>{item}</List.Item>}
+            renderItem={(item,index) => <List.Item onClick={()=>{this.removeItem(index)}}>{item}</List.Item>}
           />
         </div>
       </div>
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -3,5 +3,31 @@ const defaultState = {
   list: ["學習程式賺大錢", "拿錢用力環遊世界"],
 };
 export default (state = defaultState, actions) => {
-  return state;
+  console.log(state,actions)
+  switch (actions.type) {
+    case 'ChangeData':
+      return {
+        ...state,
+        ...{inputValue:actions.value}
+      }
+    case 'AddItem':
+      // let newData = JSON.parse(JSON.stringify(state))
+      // reducer 理只能接收state,不能改變state
+      return {
+        ...state,
+        ...{ 
+          list:[...state.list,state.inputValue],
+          inputValue:'',
+         }
+      }
+    case 'removeItem':
+      return {
+        ...state,
+        ...{ 
+          list: [...state.list].splice(actions.index,1),
+         }
+      }
+    default:
+      return state;
+  }
 };
